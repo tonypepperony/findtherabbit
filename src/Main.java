69b3eaa104471b3905cab9e9c5a5238d9de2aa63
Ruslan <rus.official@mail.ru>
2017-08-06 00:42:45 +0300

    fix code

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static int points = 0;
     private static int patron = 10;
     static int rabbitNumber;
     static int bearNumber;
 
-
-
-
-
-
-
-    public static int getPoints() {
-        return points;
-    }
-
-    public static void setPoints(int points) {
-        Main.points = points;
-    }
-
-    public static int getPatron() {
-        return patron;
-    }
-
-    public static void setPatron(int patron) {
-        Main.patron = patron;
-    }
-
     public static void main(String[] args) throws IOException {
-        Random random = new Random();
+
         int inputNumber;
-
-
-        int secretNumber = random.nextInt(100);
-        boolean win = false;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        String instruction = " [Правила игры] \n Компьютер \"загадывает\" целое число от 1 до 100, \n пользователь должен его угадать за 7 попыток (максимум),\n после каждой попытки компьютер подсказывает,\n больше ли загаданное число, или меньше.\n Важно: За неверно введенные данные вы теряете попытку. \n\n Для начала игры введите 1, для выхода из программы введите 3 \n";
+        String instruction = " [Правила игры]\nИгра длится 5 минут\n У вас есть 10 патрон\nИтд дописать\n Для начала игры введите 1, для выхода из игры введите 3 \n";
 
         //Intro
         System.out.println(" [Охота на зайца] \n >1.Начать игру \n >2.Правила \n >3.Выход" );
@@ -52,20 +26,22 @@ public class Main {
                 } else if (inputNumber == 3){
                     System.out.println("До свидания!");
                     System.exit(0);
+                } else {
+                    System.out.println("Выберите действие: 1-3");
                 }
             } catch (NumberFormatException e){
                 System.out.println("Выберите действие: 1-3");
             }
         }
 
-        //Game
+        //Start
         ArrayList<Animals> forest = createForest();
         Collections.shuffle(forest);
         rabbitNumber = checkRabbit(forest);
         bearNumber = checkBear(forest);
 
         Timer timer = new Timer();
-        timer.schedule(new StopTimeOut(), 120000);
+        timer.schedule(new StopTimeOut(), 300000);
 
         for (Animals frst : forest){
             System.out.println(frst.getKind());
@@ -160,5 +136,21 @@ public class Main {
             }
         }
         return n + 1;
+    }
+
+    public static int getPoints() {
+        return points;
+    }
+
+    public static void setPoints(int points) {
+        Main.points = points;
+    }
+
+    public static int getPatron() {
+        return patron;
+    }
+
+    public static void setPatron(int patron) {
+        Main.patron = patron;
     }
 }
